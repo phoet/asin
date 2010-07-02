ac6fe2f1fc351c4f0d1fdb378c0054d1564e6a2b
phoet <phoetmail@googlemail.com>
2010-07-02 23:47:35 +0200

    using own signing to support different urls

--- lib/asin.rb
+++ lib/asin.rb
@@ -2,47 +2,59 @@ require 'hashie'
 require 'httpclient'
 require 'crack/xml'
 require 'cgi'
+require 'base64'
 
 module ASIN
-  
+
   class Item
-    
+
     attr_reader :raw
-    
+
     def initialize(hash)
       @raw = Hashie::Mash.new(hash).ItemLookupResponse.Items.Item
     end
-    
+
     def title
       @raw.ItemAttributes.Title
     end
-    
+
   end
 
   def configure(options={})
-    # some defaults that make sense
     @options = {
-      :Service => :AWSECommerceService,
-      :XMLEscaping => :Double, # => :Single, :Double
-      :ContentType => 'text/plain;charset="UTF-8"', # trying to force utf-8
-      :ResponseGroup => :Small, # => :Small, :Medium, :Large
+      :host => 'webservices.amazon.com', 
+      :path => '/onca/xml', 
+      :digest => OpenSSL::Digest::Digest.new('sha256'),
+      :key => '', 
+      :secret => '',
     }
-    @options = @options.merge options
+    @options.merge! options
   end
-  
+
   def lookup(asin, params={})
-    Item.new call(params.merge(:Operation => :ItemLookup, :ItemId => asin))
+    Item.new(call(params.merge(:Operation => :ItemLookup, :ItemId => asin)))
   end
-  
-  BASE_URL = 'http://free.apisigning.com/onca/xml?'
-  
-  def call(params={})
+
+  def call(params)
     configure if @options.nil?
-    p url = BASE_URL + @options.merge(params).map{|key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join('&')
-    resp = HTTPClient.new.get_content(url)
-    p resp = resp.force_encoding('UTF-8') # shady workaround cause amazon returns bad utf-8 chars
-    resp = Crack::XML.parse(resp)
-    resp
+    signed = doasign(params)
+    resp = HTTPClient.new.get_content("http://#{@options[:host]}#{@options[:path]}?#{signed}")
+    resp = resp.force_encoding('UTF-8') # shady workaround cause amazon returns bad utf-8 chars
+    Crack::XML.parse(resp)
+  end
+
+  def doasign(params) # http://cloudcarpenters.com/blog/amazon_products_api_request_signing/
+    params[:Service] = :AWSECommerceService
+    params[:AWSAccessKeyId] = @options[:key]
+    params[:Timestamp] = Time.now.strftime('%Y-%m-%dT%H:%M:%SZ') # needed for signing
+    
+    query = params.map{|key, value| "#{key}=#{CGI.escape(value.to_s)}" }.sort.join('&')
+    
+    request_to_sign = "GET\n#{@options[:host]}\n#{@options[:path]}\n#{query}"
+    hmac = OpenSSL::HMAC.digest(@options[:digest], @options[:secret], request_to_sign)
+    
+    signature = CGI.escape(Base64.encode64(hmac).chomp)
+    "#{query}&Signature=#{signature}"
   end
 
 end
--- test/test_asin.rb
+++ test/test_asin.rb
@@ -9,7 +9,8 @@ class TestAsin < Test::Unit::TestCase
 
   # comment in and add your key to test real calls
   def test_r_type_from_engine
-    p item = @helper.lookup('1430218150', :AWSAccessKeyId => 'AKIAJFA5X7RTOKFNPVZQ', :ResponseGroup => :Medium)
+    @helper.configure :secret => '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', :key => 'AKIAJFA5X7RTOKFNPVZQ'
+    p item = @helper.lookup('1430218150')
     assert_match(/Learn Objective/, item.title)
   end
 
