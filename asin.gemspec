e371f35d8a4944ff8df93b39f0c3cdef982009b3
phoet <phoetmail@googlemail.com>
2010-07-03 23:51:33 +0200

    update docs

--- asin.gemspec
+++ asin.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'asin'
-  s.version = '0.0.1'
+  s.version = '0.0.2'
   s.rubyforge_project = 'none'
 
   s.author = 'Peter Schröder'
--- test/test_asin.rb
+++ test/test_asin.rb
@@ -8,10 +8,10 @@ class TestAsin < Test::Unit::TestCase
   end
 
   # comment in and add your key to test real calls
-  def test_r_type_from_engine
-    @helper.configure :secret => '4w5ApABP2dALi4/8bdqm9xIcZ8GPe0P0PnocXNTB', :key => 'AKIAJFA5X7RTOKFNPVZQ'
-    p item = @helper.lookup('1430218150')
-    assert_match(/Learn Objective/, item.title)
-  end
+  def test_r_type_from_engine  # 
+    #   @helper.configure :secret => 'your-secret', :key => 'your-key'
+    #   p item = @helper.lookup('1430218150')
+    #   assert_match(/Learn Objective/, item.title)
+    # end
 
 end
