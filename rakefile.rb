54971f4b94a241512e622b7efeb0edfb1082ef9f
phoet <phoetmail@googlemail.com>
2011-04-07 15:28:15 +0200

    cleanup

--- rakefile.rb
+++ rakefile.rb
@@ -1,20 +1,12 @@
 require "bundler"
 require "rake/rdoctask"
-require "rake/gempackagetask"
 require 'rspec/core/rake_task'
+
+Bundler::GemHelper.install_tasks
 
 RSpec::Core::RakeTask.new do |t|
   t.rspec_opts = ["--format Fuubar", "--color", "-r ./spec/spec_helper.rb"]
   t.pattern = 'spec/**/*_spec.rb'
-end
-
-Bundler::GemHelper.install_tasks
-
-spec = eval(File.new("asin.gemspec").readlines.join("\n"))
-
-Rake::GemPackageTask.new(spec) do |pkg|
-  pkg.need_zip = true
-  pkg.need_tar = true
 end
 
 Rake::RDocTask.new(:rdoc_dev) do |rd|
@@ -22,21 +14,4 @@ Rake::RDocTask.new(:rdoc_dev) do |rd|
   rd.options + ['-a', '--inline-source', '--charset=UTF-8']
 end
 
-desc "the test task"
-task :test do
-  require 'rake/testtask'
-  Rake::TestTask.new do |t|
-    t.libs << "test"
-    t.ruby_opts << "-rubygems"
-    t.test_files = FileList['test/test_*.rb']
-    t.verbose = true
-  end
-end
-task :default=>:test
-
-desc "execute build on travis-ci"
-task :travis_ci do
-  puts "WE LOOOOOOOOOOOOVE"
-  Rake::Task['spec'].invoke
-  puts "TRAVIS CI!!!!!!!!!!!!!!!oneeleven"
-end
+task :default=>:spec
