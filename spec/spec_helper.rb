a8cb9cd2147df7c38dc7d865ffccc499dbfa3fae
phoet <phoetmail@googlemail.com>
2011-08-03 09:18:22 +0200

    fixes #7

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require 'pp'
 
 ANY_ASIN            = '1430218150'
 ANY_OTHER_ASIN      = '1430216263'
-ANY_BROWSE_NODE_ID  = '163357'
+ANY_BROWSE_NODE_ID  = '599826'
 
 RSpec.configure do |c|
   c.mock_with :rspec
