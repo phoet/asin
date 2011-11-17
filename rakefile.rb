94b93b73ee1e51d910a59f7e04e2442243475421
Peter Schröder <phoetmail@googlemail.com>
2011-11-17 13:46:48 +0100

    remove failing rdoc task

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,4 @@
 require "bundler"
-require "rdoc/task"
 require "rspec/core/rake_task"
 
 Bundler::GemHelper.install_tasks
@@ -7,11 +6,6 @@ Bundler::GemHelper.install_tasks
 RSpec::Core::RakeTask.new do |t|
   t.rspec_opts = ["--format Fuubar", "--color"]
   t.pattern = 'spec/**/*_spec.rb'
-end
-
-Rake::RDocTask.new(:rdoc_dev) do |rd|
-  rd.rdoc_files.include(File.readlines('.document').map(&:strip))
-  rd.options + ['-a', '--line-numbers', '--charset=UTF-8']
 end
 
 task :default=>:spec
